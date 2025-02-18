4355fb99c314cded55bcbd4969c5a9849a37404a
Raju <smraju115@gmail.com>
2025-02-18 11:39:30 +0600

    add my all files

---
+++ AdminLTEIntregate/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdminLTEIntregate.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
